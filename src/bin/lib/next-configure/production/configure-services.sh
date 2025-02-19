6550d45a4e834b7488c85184f6f9c55c045fa363
C. M. de Picciotto <d3p1@d3p1.dev>
2025-02-19 20:59:18 +0000

    fix: add dependencies and build to platform configuration script for production environments [#1]

--- src/bin/lib/next-configure/production/configure-services.sh
+++ src/bin/lib/next-configure/production/configure-services.sh
@@ -28,11 +28,17 @@ main() {
 #         is not mounted to the service/container.
 #         Because of that, it is required to copy it
 #         from host to the service/container
+# @note   It is installed required dependencies and the build step,
+#         so when the environment is started (`docker compose up -d`)
+#         the platform works as expected
+#         (on startup the `next start` is executed)
 # @link   https://stackoverflow.com/questions/32566624/docker-cp-all-files-from-a-folder-to-existing-container-folder
 ##
 _configure_next() {
     print_message "Start Next.js configuration for prod environment" "notice"
     docker compose cp "${BASE_DOC_ROOT_DIR}/." web:/app
+    docker compose run --rm cli npm install
+    docker compose run --rm cli npm run build
     print_message "End Next.js configuration for prod environment" "notice"
 }
 
