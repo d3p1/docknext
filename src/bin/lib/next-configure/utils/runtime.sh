2a1bd2571dc8320e38b9dcfabda674eec5874865
C. M. de Picciotto <d3p1@d3p1.dev>
2026-01-03 21:08:12 +0000

    feat: implement utility that updates next scripts to use custom ones [#1]

---
+++ src/bin/lib/next-configure/utils/runtime.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+##
+# @description Utilities to configure runtime (`node` or `bun`)
+# @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
+##
+
+##
+# @note Declare required constants
+##
+declare APP_FILE="package.json"
+
+##
+# Update app scripts so they use these environment commands
+#
+# @return void
+# @note   This logic is shared by the `docknext next-install` command
+#         and the `docknext next-configure` command. That is why it is
+#         created as an utility function but inside the Next.js configuration
+#         scope. Both commands need to configure Next.js so it uses
+#         our environment variables that define our scripts
+##
+update_app_scripts() {
+    docker compose run --rm cli sed -i "s#\"dev\":.*#\"dev\": \"$DEV_CMD\"#g" "$APP_FILE"
+    docker compose run --rm cli sed -i "s#\"build\":.*#\"dev\": \"$BUILD_CMD\"#g" "$APP_FILE"
+    docker compose run --rm cli sed -i "s#\"start\":.*#\"dev\": \"$START_CMD\"#g" "$APP_FILE"
+}
