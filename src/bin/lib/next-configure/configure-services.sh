91a5be1814a7f4c61c6c056e200b767510fc77f2
C. M. de Picciotto <d3p1@d3p1.dev>
2026-01-31 01:10:45 +0000

    fix: improve configure next command so it copies production Dockerfile [#1]

--- src/bin/lib/next-configure/configure-services.sh
+++ src/bin/lib/next-configure/configure-services.sh
@@ -25,10 +25,18 @@ main() {
 #
 # @return void
 # @note   Configure custom environment scripts
+# @note   Add `Dockerfile` required for production environments.
+#         It is decided to add this step here instead of in a
+#         production scope, because this is something related to the
+#         platform source code and not to the production environment itself.
+#         Additionally, this step is executed here and not with `docknext init`,
+#         because Next.js platform should be already present and
+#         this is only guarantee with this `docknext next-configure` command
 ##
 _configure_next() {
     print_message "Start Next.js configuration" "notice"
     update_app_scripts
+    cp -R "$BASE_DIR/etc/services/web/Dockerfile.prod" "$SCRIPT_HOST_DOC_ROOT_DIR/Dockerfile"
     print_message "End Next.js configuration" "notice"
 }
 
