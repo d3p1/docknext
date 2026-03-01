77ff16ab838a36bc2e24689602d8f1030257a934
C. M. de Picciotto <d3p1@d3p1.dev>
2026-03-01 20:52:57 +0000

    fix: adjust docker compose project name so it does not use not allowed uppercase [#1]

--- src/bin/lib/up.sh
+++ src/bin/lib/up.sh
@@ -13,9 +13,9 @@
 #              we set a new project name every time the environment is started.
 #              In that way, the `docroot` volume will be prefixed with a new
 #              project name each time. Because the project name is generated
-#              using the current epoch time prefixed with `RELEASE_`,
+#              using the current epoch time prefixed with `release_`,
 #              an example of the volume name
-#              could be: `RELEASE_1772393938_docroot`.
+#              could be: `release_1772393938_docroot`.
 #              This approach has the advantage of creating new resources
 #              each time a new release is deployed,
 #              so it is easier to do rollbacks. However, this is also
@@ -42,7 +42,7 @@
 ##
 main() {
     if [ "$SCRIPT_MODE" = "production" ]; then
-        COMPOSE_PROJECT_NAME="RELEASE_$(date +%s)"
+        COMPOSE_PROJECT_NAME="release_$(date +%s)"
         export COMPOSE_PROJECT_NAME
     fi
 
